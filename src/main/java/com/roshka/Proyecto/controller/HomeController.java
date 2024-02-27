87828fce555feb65d6d8148814dde6fc372a56fb
Walter Paoli <wpaoli@roshka.com>
2024-02-27 20:06:23 -0300

    Algunas sugerencias para usar la interfaz JpaRepository

--- src/main/java/com/roshka/Proyecto/controller/HomeController.java
+++ src/main/java/com/roshka/Proyecto/controller/HomeController.java
@@ -1,21 +1,24 @@
 package com.roshka.Proyecto.controller;
 
-import com.roshka.Proyecto.repository.ReservaRepository;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Date;
-import java.sql.Time;
+import com.roshka.Proyecto.model.Reserva;
+import com.roshka.Proyecto.repository.ReservaRepository;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    ReservaRepository repo;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -23,7 +26,11 @@ public class HomeController {
     }
 
     @GetMapping("/reservas")
-    public String getReservas() {
+    public String getReservas(Model model) {
+        Optional<Reserva> optReserva = repo.findById(Integer.valueOf(1));
+        if(optReserva.isPresent()){
+            model.addAttribute("reserva", optReserva.get());
+        }
         return "reservas";
     }
 
@@ -35,11 +42,19 @@ public class HomeController {
                                @RequestParam("horario") LocalTime horario,
                                @RequestParam("cantidad") Integer cantidad,
                                @RequestParam("observaciones") String observaciones,
-                               Model m){
+                               Model m) {
         System.out.println(nombre + telefono);
+        // Validar los parametros recibidos
         //Llamar a la base de datos
+        Reserva r = new Reserva();
+        r.setNombre(nombre);
+        r.setApellido(apellido);
+        r.setTelefono(telefono);
+        //...
+        Reserva reservaGuardada = repo.save(r);
 
-        m.addAttribute("m_nombre", nombre);
+        // retornar a la pagina los datos via el model
+        m.addAttribute("m_nombre", reservaGuardada.getNombre());
         m.addAttribute("m_apellido", apellido);
         m.addAttribute("m_telefono", telefono);
         m.addAttribute("m_fecha", fecha);
